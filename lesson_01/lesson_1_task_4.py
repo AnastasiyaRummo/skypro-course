a1f53bf347cc0dcc07665cae882635132f493ec2
AnastasiyaRummo <grenaska@mail.ru>
2025-05-24 21:06:40 +0300

    правки

---
+++ lesson_01/lesson_1_task_4.py
@@ -0,0 +1,6 @@
+# 4)Создать функцию
+def print_greeting():
+    print("Привет, Мир!")
+
+
+print_greeting()
---
+++ lesson_01/урок_1/все_задания.py
@@ -0,0 +1,46 @@
+# Домашнее задание к 1 уроку
+# Задание 1.Упражнения
+
+# 1)Создать переменную
+my_name = "Настя"
+print(my_name)
+
+# 2)Перезаписать переменную
+my_age = 32
+my_age = 32 + 3
+print(my_age)
+
+# 3)Получить пользовательский ввод
+first_name = input("Введите имя:")
+last_name = input("Введите фамилию:")
+print("Меня зовут: ", last_name, first_name)
+
+
+# 4)Создать функцию
+def print_greeting():
+    print("Hello, World!")
+    print("Привет, Мир!")
+
+
+print_greeting()
+
+
+# 5)Параметризация функции
+def print_num(num):
+    print(num, end='')
+
+
+print_num('8')
+print_num('8')
+print_num('0')
+print_num('0')
+print_num('5')
+print_num('5')
+print_num('5')
+print_num('3')
+print_num('5')
+print_num('3')
+print_num('5')
+
+
+# Задание 2.Проверка кода
